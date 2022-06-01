558b7e5fcc62cfb5839e6ebb5c533079cb393e3a
karanWD <@karanshamspirzadeh@gmail.com>
2022-06-02 01:23:42 +0430

    implement base of movie db

---
+++ components/slider.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+// import required modules
+import { Pagination,Navigation } from "swiper";
+import MovieItem from "./movieItem";
+
+const Slider = ({data}) =>{
+    console.log(data)
+    return(
+        <div className={`text-white my-6`}>
+            <Swiper spaceBetween={7}
+                    slidesPerView={5.7}
+                    pagination={{clickable:true}}
+                    modules={[Navigation]}
+                    className="mySwiper">
+                {
+                    data?.map((item,index)=>{
+                        return <SwiperSlide key={index}><MovieItem data={item}/></SwiperSlide>
+                    })
+                }
+            </Swiper>
+        </div>
+    )
+}
+
+export default Slider
