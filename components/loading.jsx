131b1605a63334e50eb40ac7343dd5ca386ff9a0
kianibound <kianibound@gmail.com>
2022-06-23 12:02:50 +0430

    Uploading Whole Project

--- components/loading.jsx
+++ components/loading.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Loading = () =>{
-    return(
-        <h1 className={`text-white text-center p-24 text-xl font-bold`}>Loading ...</h1>
-    )
-}
+const Loading = () => {
+  return (
+    <h1 className={`text-white text-center p-24 text-xl font-bold`}>
+      Loading...(Please Turn On Your VPN And Wait...)
+    </h1>
+  );
+};
 
-export default Loading
+export default Loading;
--- data/staticRoutes.js
+++ data/staticRoutes.js
@@ -1,5 +1,5 @@
 export const StaticRoutes = {
-    baseUrl: "https://api.themoviedb.org/3/",
-    token: "301b07456b1452abbdda1e23bf72876d",
-    imagePath:"https://image.tmdb.org/t/p/w500"
-}
+  baseUrl: "https://api.themoviedb.org/3/",
+  token: "d69d21688e524f2e272cb84b6edaf2c6",
+  imagePath: "https://image.tmdb.org/t/p/w500",
+};
